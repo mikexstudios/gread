1a696d90ca72d4ffdf0576608b4b538ea45d3ccb
Michael Huynh <mike.huynh@gmail.com>
2013-05-17 23:15:31 -0400

    Added dashboard page and made it root.

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Gread::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'high_voltage/pages#show', :id => 'dashboard'
 
   # See how all your routes lay out with "rake routes"
 
