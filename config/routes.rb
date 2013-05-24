a2a9db80c935d57db3f552e0c8a05b497da1c084
Michael Huynh <mike.huynh@gmail.com>
2013-05-24 14:36:02 -0400

    Generated settings controller.

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Gread::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :settings
 
   # Sample resource route with options:
   #   resources :products do
