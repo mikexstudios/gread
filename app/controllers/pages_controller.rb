a7a765e8c1a487c68ced8421cb852afef7209c84
Michael Huynh <mike.huynh@gmail.com>
2013-05-25 15:17:16 -0400

    Added home page.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < HighVoltage::PagesController
+  #force_ssl :except => :all
+
+  skip_before_filter :authenticate_user!
+  before_filter :check_redirect
+
+  layout :layout_for_page
+
+  protected
+    def layout_for_page
+      case params[:id]
+      when 'home'
+        'home'
+      else
+        'application'
+      end
+    end
+
+    # The point of this filter is to check if the id is 'home'. If so, and if
+    # the user is already logged in, redirects user to the notebooks page.
+    def check_redirect
+      if params[:id] == 'home' and user_signed_in?
+        redirect_to page_path('dashboard')
+      end
+    end
+end
+
