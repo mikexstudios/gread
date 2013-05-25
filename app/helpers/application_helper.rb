5d1c1c76535e09dc7578068a2600f7eea84c69fe
Michael Huynh <mike.huynh@gmail.com>
2013-05-25 14:38:04 -0400

    Added title and location_name helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def title(page_title, include_base_title = true)
+    page_title += ' | GREAD' if include_base_title
+    content_for(:title, page_title.to_s)
+  end
+
+  # Returns a string of "action_controller" that is useful for identifying the
+  # page in CSS id/class names. 
+  def location_name
+    #Since we use high_voltage gem, the action_name will always be 'show' and the
+    #controller_name will always be pages. Thus, we need to invoke the id of the
+    #action to provide better context.
+    if controller.controller_name == 'pages'
+      return "#{params[:id]}_#{controller.controller_name}"
+    end
+
+    return "#{action_name}_#{controller.controller_name}"
+  end
 end
