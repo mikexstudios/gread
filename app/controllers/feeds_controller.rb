36c65c0c8266414d76705216202f997da4edbf9a
Michael Huynh <mike.huynh@gmail.com>
2013-07-01 10:11:22 -0400

    Crudely added subscribe to new feed page. No logic though.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,4 +2,8 @@ class FeedsController < ApplicationController
   def index
     @entries = Entry.order('created_at DESC').paginate(:page => params[:page])
   end
+
+  def new
+    @feed = Feed.new
+  end
 end
