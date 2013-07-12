e2118031cdd5f17d8268b7cbea31963f2535aedb
Michael Huynh <mike.huynh@gmail.com>
2013-07-11 23:17:44 -0400

    Used jQuery to pull JSON entry from Feed.index page.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -6,4 +6,29 @@ $(document).ready(function() {
     $target.data('collapse') ? $target.collapse('toggle') : $target.collapse();
     e.preventDefault();
   });
+
+  //When accordion elements are expanded or collapsed, we need to grab entry data
+  //through ajax or destroy the loaded content.
+  $('#items').on('show', function(e) {
+    var entry_id = '#' + e.target.id; //e.g. #entry-845
+    var id = parseInt(entry_id.split('-')[1]);
+    var url = '/entries/' + id.toString();
+    $.getJSON(url, function(data) {
+      //console.log(data);
+      $(entry_id).find('.entry-title a').attr('href', data.url).text(data.title);
+      if(data.author) {
+        $(entry_id).find('.entry-author').text('by ' + data.author);
+      }
+      $(entry_id).find('.entry-body').html(data.content);
+    });
+  });
+
+  //NOTE: The problem with this approach is that we need to manually zero out
+  //each attribute, which is not elegant.
+  $('#items').on('hidden', function(e) {
+    var entry_id = '#' + e.target.id; //e.g. #entry-845
+    $(entry_id).find('.entry-title a').attr('href', null).text(null);
+    $(entry_id).find('.entry-author').text(null);
+    $(entry_id).find('.entry-body').text(null);
+  });
 });
