0320c0ba04196052479a3ec7d10226c6cf96d78c
Michael Huynh <mike.huynh@gmail.com>
2013-07-11 22:43:18 -0400

    Entry.show can now respond with json

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,11 @@
 class EntriesController < ApplicationController
   def show
     @entry = Entry.find(params[:id])
+
+    respond_to do |format|
+      #format.html
+      format.html 
+      format.json { render :json => @entry }
+    end
   end
 end
